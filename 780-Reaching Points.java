3dc30f267423ba9dccf800ddfecffe578d270fac
Yingan Wang <yinganw2@illinois.edu>
2020-08-21 16:07:05 -0500

    reverse the sequence to find patterns

---
+++ 780-Reaching Points.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean reachingPoints(int sx, int sy, int tx, int ty) {
+        while (sx < tx && sy < ty) {
+            if (tx > ty) tx %= ty;
+            else ty %= tx;
+        }
+        return sx == tx && sy <= ty && (ty - sy) % sx == 0 ||
+            sy == ty && sx <= tx && (tx - sx) % sy == 0;
+    }
+}
