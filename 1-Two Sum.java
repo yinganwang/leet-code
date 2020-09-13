def5780f78dcc134af0307bfba17a5e5ac2c2f0d
Yingan Wang <yinganwang@Yingans-MacBook-Pro.local>
2020-09-13 00:24:54 -0500

    two-pass hash, O(n), O(n)

---
+++ 1-Two Sum.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        
+        int secondNum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            secondNum = target - nums[i];
+            if (map.containsKey(secondNum) && map.get(secondNum) != i) {
+                return new int[] {i, map.get(secondNum)};
+            }
+        }
+        return null;
+    }
+}
